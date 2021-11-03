b65fea355925393683b9eb2162adb8ac94752dd3
parkerschwantes <23pschwantes@athenian.org>
2021-11-03 12:20:54 -0700

    this commit might be the most fire

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -5,19 +5,29 @@ public class GroceryList {
     // Remember to create the array!
     // You don't need to change any of the values in the array
     public GroceryList() {
-        // YOUR CODE HERE
+        arr = new String[10];
+
     }
 
     // Adds an item to the grocery list
     public void add(String item) {
-        // YOUR CODE HERE
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] == null){
+                arr[i] = item;
+                break;
+            }
+        }
     }
 
     // Removes an item from the grocery list
     // Replaces the item with null
     // Remember to shift anything to the left if necessary
     public void remove(String item) {
-        // YOUR CODE HERE
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] == item){
+                arr[i] = null;
+            }
+        }
     }
 
     // Returns a String representation the grocery list
@@ -29,7 +39,18 @@ public class GroceryList {
     // You **may** have an extra comma at the end
     @Override
     public String toString() {
-        // YOUR CODE HERE
-        return "";
+        String groc = "";
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] != null){
+                if (arr[i + 1] == null) {
+                    groc = groc + arr[i];
+                }
+                else{
+                    groc = groc +  arr[i] + ", ";
+                }
+            }
+        }
+
+        return "Grocery List: " + groc;
     }
 }
